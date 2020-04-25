2cdd051abbea5d44d89bbf87547664153600461f
AandrzejMi <andrzej.mi@op.pl>
2020-04-25 16:11:21 +0200

    Program arguments.

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 #!python
+
+import sys
+
+strategy = sys.argv[1]
+param = sys.argv[2]
+puzzle = sys.argv[3]
+solution = sys.argv[4]
+stats = sys.argv[5]
+
 print("Hello world!")
+print(strategy)
+print(param)
+print(puzzle)
+print(solution)
+print(stats)
