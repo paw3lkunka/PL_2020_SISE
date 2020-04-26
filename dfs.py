e9748c2f09e22f845b488258688b0adba5a1e8f6
PawelKunka <pawelkun@gmail.com>
2020-04-26 20:50:42 +0200

    DFS actually works now or at least that's what it seems like

--- dfs.py
+++ dfs.py
@@ -6,27 +6,29 @@ def dfs(puzzle, order):
 
     createdStates = 1
     parsedStates = 0
-    depth = 0
+    maxDepth = 0
     oldStates = []
 
     currentStates = [puzzle]
     currentInstructions = [""]
+    currentDepths = [0]
     
     while True:
         if currentStates:
             nextState = currentStates.pop()
             nextInstructions = currentInstructions.pop()
-            if isinstance(nextState, list):
+            depth = currentDepths.pop()
+            maxDepth = max(depth, maxDepth)
+            if isinstance(nextState, list) and (nextState not in oldStates) and (depth < 16):
+                oldStates.append(nextState)
                 parsedStates += 1
-                depth = len(nextInstructions)
-                if depth < 10 and nextState not in oldStates:
-                    oldStates.append(nextState)
-                    if validate(nextState):
-                        return nextInstructions, createdStates, parsedStates, depth, (time.perf_counter() - start) * 1000 
-                    else:
-                        for operator in reversed(order):
-                            createdStates += 0
-                            currentStates.append(move(nextState, operator))
-                            currentInstructions.append(nextInstructions + operator)
+                if validate(nextState):
+                    return nextInstructions, createdStates, parsedStates, maxDepth, (time.perf_counter() - start) * 1000 
+                else:
+                    for operator in reversed(order):
+                        createdStates += 0
+                        currentStates.append(move(nextState, operator))
+                        currentInstructions.append(nextInstructions + operator)
+                        currentDepths.append(depth + 1)
         else:
-            return "Puzzle unsolved.", createdStates, parsedStates, depth, (time.perf_counter() - start) * 1000 
+            return "Puzzle unsolved.", createdStates, parsedStates, maxDepth, (time.perf_counter() - start) * 1000 
