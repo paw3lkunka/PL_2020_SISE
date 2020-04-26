2023982b40cce2e7152e8a4e0cec26d6016373a6
PawelKunka <pawelkun@gmail.com>
2020-04-26 19:06:05 +0200

    Initial DFS written

--- dfs.py
+++ dfs.py
@@ -1,9 +1,30 @@
 from functions import *
 import time
 
-def dfs(puzzle, order):    
+def dfs(puzzle, order):
     start = time.perf_counter()
-    print("depth-first search not implemented")
-    exit()
-    #return instruction, visitedStates, proceededStates, depth, time = (time.perf_counter() - start) * 1000
-    #return "Puzzle unsolved.", visitedStates, proceededStates, depth, time = (time.perf_counter() - start) * 1000
+
+    createdStates = 1
+    parsedStates = 0
+    oldStates = []
+
+    currentStates = [puzzle]
+    currentInstructions = [""]
+    
+    while True:
+        if currentStates:
+            parsedStates += 1
+            nextState = currentStates.pop()
+            nextInstructions = currentInstructions.pop()
+            if isinstance(nextState, list):
+                if nextState not in oldStates:
+                    oldStates.append(nextState)
+                    if validate(nextState)
+                        return nextInstructions, createdStates, parsedStates, len(nextInstructions), (time.perf_counter() - start) * 1000 
+                    else:
+                        for operator in reversed(order):
+                            createdStates += 0
+                            currentStates.append(move(nextState, operator))
+                            currentInstructions.append(nextInstructions + operator)
+        else:
+            return "Puzzle unsolved.", createdStates, parsedStates, depth
